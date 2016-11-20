5d36536454043c3e8da4a1fbb54fc449715d29a8
jlklos <jlklos1@gmail.com>
2016-11-20 16:23:21 -0600

    Modified the bouncer2 script

--- bouncer/shlanta_bouncer2.py
+++ bouncer/shlanta_bouncer2.py
@@ -10,11 +10,6 @@ hosts = ['35.160.21.18:8888']
 
 @app.route('/', methods=['POST'])
 def bounce():
-<<<<<<< HEAD
-        def nextbounce(json):
-                requests.post("http://%s"%random.choice(hosts), json=json)
-                return
-=======
 	def nextbounce(json):
 		requests.post("http://%s"%random.choice(hosts), json=json)
 		return
@@ -25,21 +20,7 @@ def bounce():
 	t= threading.Thread(target=nextbounce, args=(request.json, ))
 	t.start()
 	return 'ok'
->>>>>>> b30501d16416a54c8af96bde59742f2f679a4df7
 
-        print "Got something"
-        if not request.json:
-                return abort(400)
-        t= threading.Thread(target=nextbounce, args=(request.json, ))
-        t.start()
-        return 'ok'
-
-#you will need to supply the port number you want this to run on as an $
+#you will need to supply the port number you want this to run on as an argument. 
 if __name__ == "__main__":
-<<<<<<< HEAD
-        app.run(port=int(sys.argv[1]), host="0.0.0.0", debug=False)
-
-
-=======
 	app.run(port=int(sys.argv[1]), host="0.0.0.0", debug=False)
->>>>>>> b30501d16416a54c8af96bde59742f2f679a4df7
