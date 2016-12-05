0dd8946a6ed039095b86bab2a469c82cf636c96a
Mike Shlanta <sdshlanta@gmail.com>
2016-12-05 13:47:32 -0600

    comments + refactor
    fixed issue where client could fail to receive the file by running out of memory it now writes parts to the disk as they come in.

--- recieve/receive.py
+++ recieve/receive.py
@@ -2,39 +2,39 @@ from flask import Flask, request, abort
 import base64
 import time
 import sys
+import threading
 
 app = Flask(__name__)
-parts = {}
-maxParts = None
-
 @app.route('/', methods=['POST'])
 def receive():
-	print "Getting Stuff"
-	global parts
-	global maxParts
+	#here we wirte the received data to a file offset on the disk
+	def _write(data, offsset, filename):
+		#open it as a "read + write binary" file which will ensure
+		#we don't null out parts of the file which would hapen if
+		#we used used "wb"
+		with open(filename,'r+b') as fp:
+			#seek to the offset in the file
+			fp.seek(int(offsset))
+			#write the data
+			fp.write(base64.b64decode(data))
+			#make sure that data is written
+			fp.flush()
+		return
+	#debug
+	#print "Getting Stuff"
+	#make sure the request is formatted as JSON, if it isn't abort
 	if not request.json:
 		return abort(400)
-	if 'maxParts' in request.json:
-		maxParts = int(request.json['maxParts'])
-		if maxParts != None:
-			if len(parts) == maxParts+1:
-				time.sleep(1)
-				with open(sys.argv[1],'wb') as fp:
-					for index in xrange(0,len(parts)):
-						fp.write(parts[index])
-		return 'ok'
-	else:
-		parts[int(request.json['index'])] = base64.b64decode(request.json['data'])
-		if maxParts != None:
-			if len(parts) == maxParts+1:
-				time.sleep(1)
-				with open(sys.argv[1],'wb') as fp:
-					for index in xrange(0,len(parts)):
-						fp.write(parts[index])
-		return 'ok'
+	#create the thread object, we are using a thread here to ensure that the host can continue sending things
+	#(otherwise the host would have to wait the file wrote so that each host down the line
+	#could then send OK back to the next host etc.)
+	t= threading.Thread(target=_write, args=(request.json['data'],request.json['index'], sys.argv[1]))
+	#start the thread
+	t.start()
+	return 'ok'
 
 def main():
-	app.run(port=8888, host='0.0.0.0', debug=False)
+	app.run(port=8887, host='0.0.0.0', debug=False)
 
 if __name__ == '__main__':
-	main()  
+	main() 
