c93d9d7738757a16a07e314a852f8729bc8b874b
Mike Shlanta <sdshlanta@gmail.com>
2018-12-13 11:06:33 -0800

    Updated for python 3 and improved in progress feedback

--- send/sender.py
+++ send/sender.py
@@ -1,50 +1,78 @@
-import argparse #used for parsing the arguments  
-import base64 #used to encode the file so that binary files aren't garbled in transmision
-"""requests (an HTTP client lib) is used to send the information to the bouncer (and then form the bouncer to the client/other bouncers)
-while raw TCP sockets could have been used to transfer the information (and was used for the first implmentation) in the end a RESTful style HTTP
-client server archatechure was chosen to act as our method of transport. This desition was made for a few reasions I will :
-1) to send information ot the bouncers sockets could have been used but this was far quicker to prototype"""
+# used for parsing the arguments  
+import argparse
+# used to encode the file so that binary files aren't garbled in 
+# transmision 
+import base64 
+"""
+requests (an HTTP client lib) is used to send the information to the
+bouncer (and then form the bouncer to the client/other bouncers) 
+while raw TCP sockets could have been used to transfer the
+information (and was used for the first implmentation) in the end a
+RESTful style HTTP client server archatechure was chosen to act as
+our method of transport. The desition to use HTTP was made for ease
+of prototyping.
+"""
 import requests 
 import random
-import os #for getting the size of the file
+import os # for getting the size of the file
 
-#the remote bouncers that you want to hit
-remoteHosts = ['169.254.142.143:8888','169.254.142.143:8889']
-"""A generator used to split the file into parts this allows us lazily read the file
-and save on memory costs"""
+# the remote bouncers that you want to hit
+remoteHosts = [
+	'169.254.142.143:8888'
+	,'169.254.142.143:8889'
+]
+# A generator used to split the file into parts this allows us
+# lazily read the file and save on memory overhead
 def splitupFile(fileName, blockSize):
-	#open the file, this is only done once as the generator will loop around in the for section
-	fp = file(fileName)
-	#here we create an iterable out of this lambda function which reads the file for a certan number of bytes
-	#determine where in the filewe are
-	offset=fp.tell()
-	for filePart in iter(lambda: fp.read(blockSize), ''):
-		#we base64 encode the filePart for transfer
-		filePart = base64.b64encode(filePart)
-		#here we calculate the offset that the client will need to write that part of the file at.
-		yield (offset , filePart)
-		#determine where in the filewe are
-		offset=fp.tell()
+	# open the file, this is only done once as the generator will 
+	# loop around in the for section
+	with open(fileName, 'r') as fp:
+		# here we create an iterable out of this lambda function
+		# which reads the file for a certan number of bytes determine
+		# where in the filewe are
+		offset = fp.tell()
+		for filePart in iter(lambda: fp.read(blockSize), ''):
+			# we base64 encode the filePart for transfer
+			filePart = base64.b64encode(filePart)
+			# here we calculate the offset that the client will need 
+			# to write that part of the file at.
+			yield (offset , filePart)
+			# determine where in the file we are
+			offset = fp.tell()
+
 def main():
-	#debug
+	# Debug
 	print("Sending file")
-	#inform the client how large the file will be mostly so that it knows when it can stop receiving
+	# Inform the client how large the file will be mostly so that it
+	# knows when it can stop receiving
 	randomHost = random.choice(remoteHosts)
-	r = requests.post("http://%s" % randomHost, json={'filesize':str(os.path.getsize(args.fileToSend)),'blocksize':str(args.b)})
-	#here we use for's ablity to unpack tuples to unpack the tuple we yield from splitupFile as it iterates through the file
+	filesize = os.path.getsize(args.fileToSend)
+	jsonBlob = {
+		'filesize':str(filesize),
+		'blocksize':str(args.b)
+	}
+	requests.post("http://%s" % randomHost, json=jsonBlob)
+	# Here we use for's ablity to unpack tuples to unpack the tuple 
+	# we yield from splitupFile as it iterates through the file
 	for offset, part in splitupFile(args.fileToSend, args.b):
-		#setting up the json dict
-		data = {'index':offset, 'data':part}
-		#this is mostly me being lazy here I should have had a count % len(remoteHosts) so it would use each path equally but sue me I wrote this in an hour ;)
+
+		# Setting up the json dict
+		data = {
+			'index':offset,
+			'data':part
+		}
+		# pick a host, this distrbution should be uniformly random,
+		# so over a large file the hosts should be under approxmitly
+		# the ame use.
 		randomHost = random.choice(remoteHosts)
-		#here we get to the heart of the operation. the file is sent to the random host via POST encoded as a json blob
-		r = requests.post("http://%s" % randomHost, json=data)
-		#debug stuff don't worry about it
-		print("Offset %d sent to %s" % (offset, randomHost))
-		#we have covered this already
-	print("Sending finished")
+		# Here we get to the heart of the operation. the file is sent
+		# to the random host via POST encoded as a json blob
+		requests.post("http://%s" % randomHost, json=data)
+		# Show progress sending file
+		print("[%d/%d] Offset %d sent to %s" % (offset, filesize, offset, randomHost), end='\r')
+	# debug
+	print("\nSending finished")
 	randomHost = random.choice(remoteHosts)
-	#r = requests.post("http://%s" % randomHost, json={'finished':''})
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='A quick and dirty Multi-Path TCP using remote proxy hosts.')
